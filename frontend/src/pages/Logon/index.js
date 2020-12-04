5611606bf424449b20a19cb204a8e46284b3849d
kamarins <karineop@outlook.com>
2020-12-04 14:13:41 -0300

    Algumas moficações

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -10,6 +10,7 @@ import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
 export default function Logon(){
+
     const [id, setId] = useState('');
     const history = useHistory();
 
